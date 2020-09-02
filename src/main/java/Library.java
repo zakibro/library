c875732d738670a7be25d2bdbb8039d749282598
zakibro <pawelmazur835@gmail.com>
2020-09-02 19:29:02 +0200

    added additional constructor

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,7 +1,7 @@
 class Library {
 
     public static void main(String[] args) {
-        final String appName = "Library v0.3";
+        final String appName = "Library v0.4";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
                 2010, 296, "Greg", "9788373271890");
@@ -10,7 +10,7 @@ class Library {
                 352, "Helion", "9788324620845");
 
         Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
+                2008, 851, "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Available books:");
