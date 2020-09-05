c042430262b2bad70f9d62095697c862ae155aab
zakibro <pawelmazur835@gmail.com>
2020-09-05 19:06:50 +0200

    added table

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -5,21 +5,23 @@ import app.Book;
 class Library {
 
     public static void main(String[] args) {
-        final String appName = "model.Library v0.5";
+        final String appName = "Library v0.7";
 
-        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
                 2010, 296, "Greg", "9788373271890");
 
-        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009,
+        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009,
                 352, "Helion", "9788324620845");
 
-        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
+        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
                 2008, 851, "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Available books:");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
     }
 }
